efa70e36dd2c72fc235eb63c083862fec4416e88
Phong Le <phongnguyen.le22@gmail.com>
2022-03-26 15:15:52 +0800

    Updated demo.py

--- demo.py
+++ demo.py
@@ -5,23 +5,28 @@ while True:
   
   if controls.is_up_pressed():
     print("up")
+    sleep(0.2)
 
   if controls.is_down_pressed():
     print("down")
+    sleep(0.2)
 
   if controls.is_left_pressed():
     print("left")
+    sleep(0.2)
 
   if controls.is_right_pressed():
     print("right")
+    sleep(0.2)
 
   if controls.is_center_pressed():
     print("center")
+    sleep(0.2)
 
   if controls.is_A_pressed():
     print("A")
+    sleep(0.2)
 
   if controls.is_B_pressed():
     print("B")
-
-  sleep(0.5)
+    sleep(0.2)
