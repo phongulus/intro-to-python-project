8c7702062a20a27327c70025d0c21b8083731045
martinl5 <102524041+martinl5@users.noreply.github.com>
2022-04-06 16:29:05 +0800

    Update food.py
    documentation

--- model/food.py
+++ model/food.py
@@ -5,6 +5,21 @@ import random
 
 class Food:
     def __init__(self, width: int, height: int):
+        """
+        Initializes the Food object and sets its attributes
+        of color and position and points
+
+        Parameters:
+        ----
+        width<int>: width of grid
+        height<int>: height of grid
+
+        Attributes:
+        ----
+        color<Color>: RGB value of food (Red)
+        position<Point>: Position of food
+        all_points<list(Point)>: List of points of all points in the grid
+        """
         self.color: Color = Color(255, 0, 0)  # Red
         self.position: Point = Point(1, 1)  # TODO: Randomize this
 
@@ -13,7 +28,15 @@ class Food:
             for y in range(height):
                 self.all_points.append(Point(x, y))
 
-    def move_random_position(self, snake_pos) -> None:
+    def move_random_position(self, snake_pos: deque[Point]) -> None:
+        """
+        Moves the food to a random position that is not it's old position
+        and not in the snake
+
+        Paramters:
+        ----
+        snake_pos<deque>: snake points
+        """
         # Todo: food cannot appear within the snake body
         self.position = random.choice(self.all_points)
 
