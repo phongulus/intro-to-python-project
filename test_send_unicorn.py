68cd55fea40033e729d005bf6474d954c3cd7369
Nay <naychi.w_naing@u.yale-nus.edu.sg>
2022-04-06 17:53:33 +0800

    test for bytes_to_matrix

--- test_send_unicorn.py
+++ test_send_unicorn.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 
-class SnakeTestCase(TestCase):
+class UnicornTestCase(TestCase):
 
     def test_matrix_to_bytes(self):
         from send_unicorn import matrix_to_bytes
@@ -23,8 +23,25 @@ class SnakeTestCase(TestCase):
             input3.append(sub_list)
             sub_list = []
 
-        output3 = matrix_to_bytes([[(0, 0, 0), (10, 20, 30)],
-                                   [(255, 0, 255), (0, 255, 0)]])
+        output3 = matrix_to_bytes(input3)
         assert type(output1) == bytes
         assert type(output2) == bytes
         assert type(output3) == bytes
+
+    def test_bytes_to_matrix(self):
+        from send_unicorn import bytes_to_matrix
+        from random import randbytes
+        output1 = bytes_to_matrix(b'\x00\x00\x00', 1, 1)
+        assert output1 == [[(0, 0, 0)]]
+        output2 = bytes_to_matrix(
+            b'\x00\x00\x00\n\x14\x1e\xff\x00\xff\x00\xff\x00', 2, 2)
+        assert output2 == [
+            [(0, 0, 0), (10, 20, 30)],
+            [(255, 0, 255), (0, 255, 0)]
+        ]
+
+        input3 = randbytes(192)
+        output3 = bytes_to_matrix(input3, 8, 8)
+        assert type(output1) == list
+        assert type(output2) == list
+        assert type(output3) == list
