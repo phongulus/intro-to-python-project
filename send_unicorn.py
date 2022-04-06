0cdbbc5060fe634e956bc8d4f88550362313cf5e
Phong Le <phongnguyen.le22@gmail.com>
2022-04-06 16:16:07 +0800

    Update send_unicorn

--- send_unicorn.py
+++ send_unicorn.py
@@ -4,28 +4,56 @@ import socket
 UDP_IP = "192.168.43.200"
 UDP_PORT = 9000
 
-# print("UDP target IP: %s" % UDP_IP)
-# print("UDP target port: %s" % UDP_PORT)
-# print("message: %s" % MESSAGE)
- 
-# sock = socket.socket(socket.AF_INET, # Internet
-#                      socket.SOCK_DGRAM) # UDP
-# sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+def matrix_to_bytes(mat: list[list[tuple[int, int, int]]]) -> bytes:
+    """
+    Convert an (r, g, b) matrix to a sequence of bytes that can be
+    sent via UDP to the Raspberry Pi with the LED matrix.
 
-def send_matrix_data(mat):
+    Parameters:
+    ----
+    `mat <list[list[tuple[int, int, int]]]>`: The rows * columns matrix
+    where each element is a tuple representing the color of a pixel via
+    RGB code in a tuple `(r, g, b)`.
+
+    Return:
+    ----
+    `data <bytes>`: The sequence of bytes, of length rows * columns.
     """
-    Send the LED matrix color data to the remote UnicornHat.
-    Takes `mat` as an argument, which is a 2D list of tuples
-    `(r, g, b)`.
-    """
-    to_send = b''
+
+    data = b''
     for y in mat:
         for x in y:
             to_send += x[0].to_bytes(1, 'little')
             to_send += x[1].to_bytes(1, 'little')
             to_send += x[2].to_bytes(1, 'little')
+    return data
+
+
+def send_matrix_data(mat: list[list[tuple[int, int, int]]]) -> None:
+    """
+    Send the LED matrix color data to the remote UnicornHat.
+    Takes `mat` as an argument, which is a 2D list of tuples
+    `(r, g, b)`.
+
+    Parameters:
+    ----
+    `mat <list[list[tuple[int, int, int]]]>`: The rows * columns matrix
+    where each element is a tuple representing the color of a pixel via
+    RGB code in a tuple `(r, g, b)`.
+
+    Return:
+    ----
+    None
+    """
+
+    to_send = matrix_to_bytes(mat)
     sock = socket.socket(socket.AF_INET, # Internet
                         socket.SOCK_DGRAM) # UDP
+
+    # Send matrix data three times to make sure that the LED matrix
+    # Raspberry Pi receives it.
     sock.sendto(to_send, (UDP_IP, UDP_PORT))
     sock.sendto(to_send, (UDP_IP, UDP_PORT))
     sock.sendto(to_send, (UDP_IP, UDP_PORT))
+
+    return None
