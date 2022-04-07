753a21654941eedfe6e241fac19105df1b16b5d5
Phong Le <phongnguyen.le22@gmail.com>
2022-04-08 04:56:18 +0800

    Added descriptions for Game

--- model/game.py
+++ model/game.py
@@ -6,12 +6,43 @@ from model.util import Color
 class Game:
 
     def __init__(self, width: int, height: int):
+        """
+        Initializes the Game object and sets its attributes
+        width and height. Also creates a Snake object and a
+        Food object.
+
+        Parameters:
+        ----
+        width<int>: width of grid.
+        height<int>: height of grid.
+
+        Attributes:
+        ----
+        width<int: width of grid.
+        height<int>: height of grid.
+        snake<Snake>: Snake object to keep track of snake length and position.
+        food<Food>: Food object to keep track of food position.
+        """
+
         self.snake = Snake(width, height)
         self.food = Food(width, height)
         self.width = width
         self.height = height
 
     def update_game(self, last_event) -> None:
+        """
+        Update the game state (Snake and Food) based on player input
+        (or lack thereof).
+
+        Parameters:
+        ----
+        last_event<Any>: type None, when there are no inputs events to
+        process. Type Direction, when there is player input.
+
+        Return:
+        ----
+        None; game state is modified in-place.
+        """
 
         if last_event:
             self.snake.turn(last_event)
@@ -26,7 +57,20 @@ class Game:
             self.food.reset()
 
     def get_matrix(self) -> list[list[tuple[int, int, int]]]:
-        
+        """
+        Get the current game state as a matrix for display on the LED matrix.
+
+        Parameters:
+        ----
+        None
+
+        Return:
+        ----
+        mat<list[list[tuple[int, int, int]]]>: The height * width matrix
+        where each element is a tuple representing the color of a pixel via
+        RGB code in a tuple `(r, g, b)`.
+        """
+
         # Make a blank width * height matrix.
         mat = []
         for _ in range(self.height):
